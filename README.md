a71819f989ee8fbe7e28300e4aa3b80dcc4b0f37
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-06-07 21:24:57 +0100

    FInish react client

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# microservices-with-nodejs-react
+# Build, deploy, and scale an E-Commerce app using Microservices built with Node, React, Docker and Kubernetes
---
+++ client/src/CommentCreate.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import axios from 'axios';
+
+export default ({ postId }) => {
+    const [ comment, setComment ] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+           comment
+        });
+        setComment('');
+    };
+
+    return <div>
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label>New Comment</label>
+                <input className="form-control" value={comment} onChange={e => setComment(e.target.value)}/>
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+};
